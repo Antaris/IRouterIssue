faadd1e6b656ff41d96fb33e76b64d27ca3541aa
Matthew Abbott <matthew.abbott@freshegg.com>
2016-04-28 13:13:03 +0100

    Initial commit

---
+++ CustomRouter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Mvc.Infrastructure;
+using Microsoft.AspNet.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IRouterIssue
+{
+    public class CustomRouter : IRouter
+    {
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            return null; // NOOP
+        }
+
+        public Task RouteAsync(RouteContext context)
+        {
+            var handler = new MvcRouteHandler();
+
+            context.RouteData.Values.Add("controller", "Home");
+            context.RouteData.Values.Add("action", "Index");
+
+            return handler.RouteAsync(context);
+        }
+    }
+}
